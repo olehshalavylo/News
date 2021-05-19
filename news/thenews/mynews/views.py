81ef72e1d8300fa0db1bcbcde20832fb54099ed2
olehshalavylo <61602366+olehshalavylo@users.noreply.github.com>
2021-05-19 10:04:18 +0300

    Second version
    Sasha`s commit

--- news/thenews/mynews/views.py
+++ news/thenews/mynews/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+from .forms import PostForm, EditForm
+from django.urls import reverse_lazy
 
 #def home(request):
 #	return render(request, 'home.html', {})
 
+
 class HomeView(ListView):
 	model = Post
 	template_name = 'home.html'
+	ordering = ['-post_date']
+	#ordering = ['-id']
 
 
 class ArticleDetailView(DetailView):
@@ -17,6 +22,20 @@ class ArticleDetailView(DetailView):
 
 class AddPostView(CreateView):
 	model = Post
+	form_class = PostForm
 	template_name = 'add_post.html'
-	fields = '__all__'
+	#fields = '__all__'
 	#fields = ('title', 'body')
+
+
+class UpdatePostView(UpdateView):
+	model = Post
+	form_class = EditForm
+	template_name = 'update_post.html'
+	#fields = ['title', 'title_tag', 'body']
+
+
+class DeletePostView(DeleteView):
+	model = Post
+	template_name = 'delete_post.html'
+	success_url = reverse_lazy('home')
