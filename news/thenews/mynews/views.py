9a41d1426f2ac57a139e1b8573b10edfa79a8cc6
AlexTensai <alexdemien137@gmail.com>
2021-06-04 03:06:47 +0300

    Categories
    Categories

--- news/thenews/mynews/views.py
+++ news/thenews/mynews/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post
+from .models import Post, Category
 from .forms import PostForm, EditForm
 from django.urls import reverse_lazy
 
@@ -11,13 +11,34 @@ from django.urls import reverse_lazy
 class HomeView(ListView):
 	model = Post
 	template_name = 'home.html'
+	cats = Category.objects.all()
 	ordering = ['-post_date']
 	#ordering = ['-id']
+
+	def get_context_data(self, *args, **kwargs):
+		cat_menu = Category.objects.all()
+		context = super(HomeView, self).get_context_data(*args, **kwargs)
+		context["cat_menu"] = cat_menu
+		return context
+
+def CategoryListView(request):
+	cat_menu_list = Category.objects.all()
+	return render(request, 'category_list.html', {'cat_menu_list':cat_menu_list})
+
+def CategoryView(request, cats):
+	category_posts = Post.objects.filter(category=cats.replace('-', ' '))
+	return render(request, 'categories.html', {'cats':cats.title().replace('-', ' '), 'category_posts':category_posts})
 
 
 class ArticleDetailView(DetailView):
 	model = Post
 	template_name = 'article_details.html'
+
+	def get_context_data(self, *args, **kwargs):
+		cat_menu = Category.objects.all()
+		context = super(ArticleDetailView, self).get_context_data(*args, **kwargs)
+		context["cat_menu"] = cat_menu
+		return context
 
 
 class AddPostView(CreateView):
@@ -25,6 +46,13 @@ class AddPostView(CreateView):
 	form_class = PostForm
 	template_name = 'add_post.html'
 	#fields = '__all__'
+	#fields = ('title', 'body')
+
+class AddCategoryView(CreateView):
+	model = Category
+	#form_class = PostForm
+	template_name = 'add_category.html'
+	fields = '__all__'
 	#fields = ('title', 'body')
 
 
--- news/thenews/thenews/settings.py
+++ news/thenews/thenews/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
